ea41c548f9eb01e5d4abf390010a54967a848433
JaJasiok <jasiek.k01@gmail.com>
2023-11-08 20:08:19 +0100

    Works on data presentation #2

---
+++ app/src/main/java/com/example/meteomind/HourlyWindAdapter.kt
@@ -0,0 +1,60 @@
+package com.example.meteomind
+
+import android.content.Context
+import android.graphics.drawable.Drawable
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.cardview.widget.CardView
+import androidx.recyclerview.widget.RecyclerView
+
+class HourlyWindAdapter(private var list: List<Int>) : RecyclerView.Adapter<HourlyWindAdapter.ViewHolder>() {
+
+    private var listener: Listener? = null
+
+    interface Listener {
+        fun onClick(position: Int)
+    }
+
+    fun setListener(listener: Listener?) {
+        this.listener = listener
+    }
+
+    class ViewHolder(itemView: CardView) : RecyclerView.ViewHolder(itemView) {
+        var cardView: CardView = itemView
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        val cv = LayoutInflater.from(parent.context)
+            .inflate(R.layout.card_wind, parent, false) as CardView
+        return ViewHolder(cv)
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val cardView = holder.cardView
+
+//        cardView.findViewById<TextView>(R.id.temp_text).text = "21°C"
+//
+//        cardView.findViewById<ImageView>(R.id.hourly_image).setImageDrawable(getDrawableByName(cardView.context, "cloud.xml"))
+//
+//        cardView.findViewById<TextView>(R.id.card_hour).text = "12:00"
+
+        cardView.setOnClickListener {
+            listener?.onClick(position)
+        }
+    }
+
+    override fun getItemCount(): Int {
+        return list.size
+    }
+
+    fun getDrawableByName(context: Context, drawableName: String): Drawable? {
+        val resources = context.resources
+        val packageName = context.packageName
+        val resourceId = resources.getIdentifier(drawableName, "drawable", packageName)
+        return if (resourceId != 0) {
+            resources.getDrawable(resourceId, null)
+        } else {
+            null
+        }
+    }
+}
